3e817ea9cf9ec6aff8fb1dce15a51165af340b7b
Marijn Haverbeke <marijnh@gmail.com>
2015-03-11 12:23:02 +0100

    Belatedly add defaultkeymap.js

---
+++ src/defaultkeymap.js
@@ -0,0 +1,11 @@
+export default {
+  "Enter": "endBlock",
+  "Ctrl-Enter": "insertHardBreak",
+  "Ctrl-B": "toggleStrong",
+  "Ctrl-I": "toggleEm",
+  "Backspace": "delBackward",
+  "Delete": "delForward",
+  "Ctrl-Z": "undo",
+  "Ctrl-Y": "redo",
+  "Shift-Ctrl-Z": "redo"
+}
