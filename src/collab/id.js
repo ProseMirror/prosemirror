491655c377705c4b32584d90480d4096982a5e39
Marijn Haverbeke <marijnh@gmail.com>
2015-04-24 15:04:45 +0200

    Use numeric ids, define non-commutative parent/child id derivation
    Add tests

--- src/collab/id.js
+++ src/collab/id.js
@@ -1,19 +1,44 @@
-function pad(str, len) {
-  while (str.length < len) str = "0" + str
-  return str
-}
+// Generating and manipulating 48-bit ID values
+// Stored as JS numbers
+
+const two48 = Math.pow(2, 48)
+const max48 = two48 - 1
+const two24 = Math.pow(2, 24)
+const max24 = two24 - 1
 
 export function randomID() {
-  return pad(Math.floor(Math.random() * 0xffffffffff).toString(16), 10)
+  return Math.floor(Math.random() * max48)
 }
+
+function lo24(id) { return id & max24 }
+// Can't use bitwise operators to get at higher bits, because those
+// clamp their operands to 32 bits
+function hi24(id) { return Math.floor(id / two24) }
+
+function hilo(hi, lo) { return hi * two24 + lo }
 
 export function xorIDs(a, b) {
-  let hiA = parseInt(a.slice(0, 5), 16)
-  let loA = parseInt(a.slice(5), 16)
-  let hiB = parseInt(b.slice(0, 5), 16)
-  let loB = parseInt(b.slice(5), 16)
-  return pad((hiA ^ hiB).toString(16), 5) +
-         pad((loA ^ loB).toString(16), 5)
+  return hilo(hi24(a) ^ hi24(b), lo24(a) ^ lo24(b))
 }
 
-export const nullID = "0000000000"
+const bit47 = Math.pow(2, 47)
+
+export function rotateIDLeft(id) {
+  let bit = id % 2
+  return (id - bit) / 2 + bit * bit47
+}
+
+export function rotateIDRight(id) {
+  let bit = id >= bit47
+  return (id - bit * bit47) * 2 + bit
+}
+
+export function childID(parent, change) {
+  return rotateIDLeft(xorIDs(parent, change))
+}
+
+export function parentID(child, change) {
+  return xorIDs(rotateIDRight(child), change)
+}
+
+export const nullID = 0
