b6df397ab56632f1827728e3531d278835ffca14
Marijn Haverbeke <marijnh@gmail.com>
2015-06-04 14:16:56 +0200

    Keep sorted array of marked ranges, rather than re-sorting for every redraw

--- src/edit/range.js
+++ src/edit/range.js
@@ -15,14 +15,67 @@ export class MarkedRange {
 
 eventMixin(MarkedRange)
 
+class RangeSorter {
+  constructor() {
+    this.sorted = []
+  }
+
+  find(at) {
+    let min = 0, max = this.sorted.length
+    for (;;) {
+      if (max < min + 10) {
+        for (let i = min; i < max; i++)
+          if (this.sorted[i].at.cmp(at) >= 0) return i
+        return max
+      }
+      let mid = (min + max) >> 1
+      if (this.sorted[mid].cmp(at) > 0) max = mid
+      else min = mid
+    }
+  }
+
+  insert(obj) {
+    this.sorted.splice(this.find(obj.at), 0, obj)
+  }
+
+  remove(at, range) {
+    let pos = this.find(at)
+    for (let dist = 0;; dist++) {
+      let leftPos = pos - dist - 1, rightPos = pos + dist
+      if (leftPos >= 0 && this.sorted[leftPos].range == range) {
+        this.sorted.splice(leftPos, 1)
+        return
+      } else if (rightPos < this.sorted.length && this.sorted[rightPos].range == range) {
+        this.sorted.splice(rightPos, 1)
+        return
+      }
+    }
+  }
+
+  resort() {
+    for (let i = 0; i < this.sorted.length; i++) {
+      let cur = this.sorted[i]
+      let at = cur.at = cur.type == "open" ? cur.range.from : cur.range.to
+      let pos = i
+      while (pos > 0 && this.sorted[pos - 1].at.cmp(at) > 0) {
+        this.sorted[pos] = this.sorted[pos - 1]
+        this.sorted[--pos] = cur
+      }
+    }
+  }
+}
+
 export class RangeStore {
   constructor(pm) {
     this.pm = pm
     this.ranges = []
+    this.sorted = new RangeSorter
   }
 
   addRange(range) {
     this.ranges.push(range)
+    this.sorted.insert({type: "open", at: range.from, range: range})
+    this.sorted.insert({type: "close", at: range.to, range: range})
     this.markDisplayDirty(range)
   }
 
@@ -30,6 +83,8 @@ export class RangeStore {
     let found = this.ranges.indexOf(range)
     if (found > -1) {
       this.ranges.splice(found, 1)
+      this.sorted.remove(range.from, range)
+      this.sorted.remove(range.to, range)
       this.markDisplayDirty(range)
       range.clear()
     }
@@ -48,6 +103,7 @@ export class RangeStore {
         range.to = range.from
       }
     }
+    this.sorted.resort()
   }
 
   markDisplayDirty(range) {
@@ -78,15 +134,7 @@ export class RangeStore {
   }
 
   activeRangeTracker() {
-    let sorted = []
-    for (let i = 0; i < this.ranges.length; i++) {
-      let range = this.ranges[i]
-      if (!range.options.className) continue
-      sorted.push({type: "open", at: range.from, className: range.options.className})
-      sorted.push({type: "close", at: range.to, className: range.options.className})
-    }
-    sorted.sort((a, b) => a.at.cmp(b.at))
-    return new RangeTracker(sorted)
+    return new RangeTracker(this.sorted.sorted)
   }
 }
 
@@ -100,18 +148,26 @@ class RangeTracker {
   advanceTo(pos) {
     let next
     while (this.pos < this.sorted.length && (next = this.sorted[this.pos]).at.cmp(pos) <= 0) {
+      let className = next.range.options.className
+      if (!className) continue
       if (next.type == "open")
-        this.current.push(next.className)
+        this.current.push(className)
       else
-        this.current.splice(this.current.indexOf(next.className), 1)
+        this.current.splice(this.current.indexOf(className), 1)
       this.pos++
     }
   }
 
   nextChangeBefore(pos) {
-    if (this.pos == this.sorted.length) return null
-    let next = this.sorted[this.pos]
-    if (next.at.cmp(pos) >= 0) return null
-    return next.at.offset
+    for (;;) {
+      if (this.pos == this.sorted.length) return null
+      let next = this.sorted[this.pos]
+      if (!next.range.options.className)
+        this.pos++
+      else if (next.at.cmp(pos) >= 0)
+        return null
+      else
+        return next.at.offset
+    }
   }
 }
