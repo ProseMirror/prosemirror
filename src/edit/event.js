3db4d71dae7ff10f84d2b495d06f83d8b2b53105
Marijn Haverbeke <marijnh@gmail.com>
2015-09-08 12:31:16 +0200

    Rewrite event mixin, add signalHandleable

--- src/edit/event.js
+++ src/edit/event.js
@@ -1,31 +1,41 @@
-export function addEventListener(emitter, type, f) {
-  let map = emitter._handlers || (emitter._handlers = {})
-  let arr = map[type] || (map[type] = [])
-  arr.push(f)
-}
+const methods = {
+  on(type, f) {
+    let map = this._handlers || (this._handlers = {})
+    let arr = map[type] || (map[type] = [])
+    arr.push(f)
+  },
 
-export function removeEventListener(emitter, type, f) {
-  let arr = emitter._handlers && emitter._handlers[type]
-  if (arr) for (let i = 0; i < arr.length; ++i)
-    if (arr[i] == f) { arr.splice(i, 1); break }
-}
+  off(type, f) {
+    let arr = this._handlers && this._handlers[type]
+    if (arr) for (let i = 0; i < arr.length; ++i)
+      if (arr[i] == f) { arr.splice(i, 1); break }
+  },
 
-export function signal(emitter, type, ...values) {
-  let arr = emitter._handlers && emitter._handlers[type]
-  if (arr) for (let i = 0; i < arr.length; ++i)
-    arr[i](...values)
-}
+  signal(type, ...values) {
+    let arr = this._handlers && this._handlers[type]
+    if (arr) for (let i = 0; i < arr.length; ++i)
+      arr[i](...values)
+  },
 
-export function hasHandler(emitter, type) {
-  let arr = emitter._handlers && emitter._handlers[type]
-  return arr && arr.length > 0
+  signalHandleable(type, ...values) {
+    let arr = this._handlers && this._handlers[type]
+    if (arr) for (let i = 0; i < arr.length; ++i) {
+      let  result = arr[i](...values)
+      if (result !== false) return result
+    }
+    return false
+  },
+
+  hasHandler(type) {
+    let arr = this._handlers && this._handlers[type]
+    return arr && arr.length > 0
+  }
 }
 
 // Add event-related methods to a constructor's prototype, to make
 // registering events on such objects more convenient.
 export function eventMixin(ctor) {
   let proto = ctor.prototype
-  proto.on = proto.addEventListener = function(type, f) { addEventListener(this, type, f) }
-  proto.off = proto.removeEventListener = function(type, f) { removeEventListener(this, type, f) }
-  proto.signal = function(type, ...values) { signal(this, type, ...values) }
+  for (var prop in methods) if (methods.hasOwnProperty(prop))
+    proto[prop] = methods[prop]
 }
