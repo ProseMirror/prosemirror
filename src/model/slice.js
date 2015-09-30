c972740dabab55010fcfff5099784de3aa91bb95
Marijn Haverbeke <marijnh@gmail.com>
2015-09-30 17:04:26 +0200

    Remove destructive node changes in model/slice

--- src/model/slice.js
+++ src/model/slice.js
@@ -1,75 +1,27 @@
 import {Node, findConnection} from "./node"
 
-function copyInlineTo(node, offset, copy) {
-  for (let left = offset, i = 0; left > 0; i++) {
-    let chunk = node.content[i]
-    if (chunk.text.length <= left) {
-      left -= chunk.text.length
-      copy.push(chunk)
-    } else {
-      copy.push(chunk.slice(0, left))
-      break
-    }
-  }
-}
-
-function copyInlineFrom(node, offset, copy) {
-  for (let before = offset, i = 0; i < node.content.length; i++) {
-    let chunk = node.content[i]
-    if (before == 0) {
-      copy.push(chunk)
-    } else if (chunk.text.length <= before) {
-      before -= chunk.text.length
-    } else {
-      copy.push(chunk.slice(before))
-      before = 0
-    }
-  }
-}
-
-function copyInlineBetween(node, from, to, copy) {
-  if (from == to) return
-
-  for (let pos = 0, i = 0; pos < to; i++) {
-    var chunk = node.content[i], size = chunk.text.length
-    if (pos < from) {
-      if (pos + size > from)
-        copy.push(chunk.slice(from - pos, Math.min(to - pos, size)))
-    } else if (pos + size <= to) {
-      copy.push(chunk)
-    } else if (pos < to) {
-      copy.push(chunk.slice(0, to - pos))
-    }
-    pos += size
-  }
-}
-
 export function sliceBefore(node, pos, depth = 0) {
-  let copy = node.copy()
+  let content
   if (depth < pos.depth) {
     let n = pos.path[depth]
-    copy.pushFrom(node, 0, n)
-    copy.push(sliceBefore(node.content[n], pos, depth + 1))
-  } else if (node.type.contains != "span") {
-    copy.pushFrom(node, 0, pos.offset)
+    content = node.slice(0, n)
+    content.push(sliceBefore(node.content[n], pos, depth + 1))
   } else {
-    copyInlineTo(node, pos.offset, copy)
+    content = node.slice(0, pos.offset)
   }
-  return copy
+  return node.copy(content)
 }
 
 export function sliceAfter(node, pos, depth = 0) {
-  let copy = node.copy()
+  let content
   if (depth < pos.depth) {
     let n = pos.path[depth]
-    copy.push(sliceAfter(node.content[n], pos, depth + 1))
-    copy.pushFrom(node, n + 1)
-  } else if (node.type.contains != "span") {
-    copy.pushFrom(node, pos.offset)
+    content = node.slice(n + 1)
+    content.unshift(sliceAfter(node.content[n], pos, depth + 1))
   } else {
-    copyInlineFrom(node, pos.offset, copy)
+    content = node.slice(pos.offset)
   }
-  return copy
+  return node.copy(content)
 }
 
 export function sliceBetween(node, from, to, collapse = true, depth = 0) {
@@ -82,22 +34,24 @@ export function sliceBetween(node, from, to, collapse = true, depth = 0) {
     for (let i = conn.length - 1; i >= 0; i--) inner = new Node(conn[i], null, [inner])
     return node.copy([inner])
   } else {
-    var copy = node.copy()
+    let content
     if (depth == from.depth && depth == to.depth && node.type.block) {
-      copyInlineBetween(node, from.offset, to.offset, copy)
+      content = node.slice(from.offset, to.offset)
     } else {
+      content = []
       let start
       if (depth < from.depth) {
         start = from.path[depth] + 1
-        copy.push(sliceAfter(node.content[start - 1], from, depth + 1))
+        content.push(sliceAfter(node.content[start - 1], from, depth + 1))
       } else {
         start = from.offset
       }
       let end = depth < to.depth ? to.path[depth] : to.offset
-      copy.pushFrom(node, start, end)
+      let between = node.slice(start, end)
+      for (let i = 0; i < between.length; i++) content.push(between[i])
       if (depth < to.depth)
-        copy.push(sliceBefore(node.content[end], to, depth + 1))
+        content.push(sliceBefore(node.content[end], to, depth + 1))
     }
-    return copy
+    return node.copy(content)
   }
 }
