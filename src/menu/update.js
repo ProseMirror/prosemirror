123bf5ce309c8b0a5dde6298b7b99dcbd95209ed
Marijn Haverbeke <marijnh@gmail.com>
2015-11-26 14:13:09 +0100

    Make menu updates fire even when no flush happens, debounce flushes

--- src/menu/update.js
+++ src/menu/update.js
@@ -1,3 +1,6 @@
+const MIN_FLUSH_DELAY = 200
+const UPDATE_TIMEOUT = 200
+
 export class MenuUpdate {
   constructor(pm, events, prepare) {
     this.pm = pm
@@ -5,22 +8,29 @@ export class MenuUpdate {
 
     this.mustUpdate = false
     this.updateInfo = null
+    this.timeout = null
+    this.lastFlush = 0
 
     this.events = events.split(" ")
-    this.onEvent = () => this.mustUpdate = true
+    this.onEvent = this.onEvent.bind(this)
+    this.force = this.force.bind(this)
     this.events.forEach(event => pm.on(event, this.onEvent))
     pm.on("flush", this.onFlush = this.onFlush.bind(this))
     pm.on("flushed", this.onFlushed = this.onFlushed.bind(this))
   }
 
   detach() {
+    clearTimeout(this.timeout)
     this.events.forEach(event => this.pm.off(event, this.onEvent))
     this.pm.off("flush", this.onFlush)
     this.pm.off("flushed", this.onFlushed)
   }
 
   onFlush() {
-    if (this.mustUpdate) {
+    let now = Date.now()
+    if (this.mustUpdate && (now - this.lastFlush) >= MIN_FLUSH_DELAY) {
+      this.lastFlush = now
+      clearTimeout(this.timeout)
       this.mustUpdate = false
       this.update = this.prepare()
     }
@@ -33,9 +43,17 @@ export class MenuUpdate {
     }
   }
 
+  onEvent() {
+    this.mustUpdate = true
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(this.force, UPDATE_TIMEOUT)
+  }
+
   force() {
     this.mustUpdate = false
     this.updateInfo = null
+    this.lastFlush = Date.now()
+    clearTimeout(this.timeout)
     let update = this.prepare()
     if (update) update()
   }
