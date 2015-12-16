e2b95308f4b6e7624057d22e9805a31874d219a6
Marijn Haverbeke <marijnh@gmail.com>
2015-12-16 17:01:24 +0100

    Make autoinput work again
    It was passing strings where NodeTypes were expected

--- src/inputrules/autoinput.js
+++ src/inputrules/autoinput.js
@@ -7,6 +7,9 @@ defineOption("autoInput", false, function(pm, val, old) {
   else if (!val && old) removeInputRules(pm, rules)
 })
 
+// FIXME attach node-specific rules to node types, rather than
+// hard-coding the node names.
+
 export var rules = [
   new Rule("-", /--$/, "—"),
   new Rule('"', /\s(")$/, "“"),
@@ -15,23 +18,23 @@ export var rules = [
   new Rule("'", /'$/, "’"),
 
   new Rule(" ", /^\s*> $/, function(pm, _, pos) {
-    wrapAndJoin(pm, pos, "blockquote")
+    wrapAndJoin(pm, pos, pm.schema.nodeType("blockquote"))
   }),
   new Rule(" ", /^(\d+)\. $/, function(pm, match, pos) {
     let order = +match[1]
-    wrapAndJoin(pm, pos, "ordered_list", {order: order || null, tight: true},
+    wrapAndJoin(pm, pos, pm.schema.nodeType("ordered_list"), {order: order || null},
                 node => node.size + (node.attrs.order || 1) == order)
   }),
   new Rule(" ", /^\s*([-+*]) $/, function(pm, match, pos) {
     let bullet = match[1]
-    wrapAndJoin(pm, pos, "bullet_list", {bullet: bullet, tight: true},
+    wrapAndJoin(pm, pos, pm.schema.nodeType("bullet_list"), null,
                 node => node.attrs.bullet == bullet)
   }),
   new Rule("`", /^```$/, function(pm, _, pos) {
-    setAs(pm, pos, "code_block", {params: ""})
+    setAs(pm, pos, pm.schema.nodeType("code_block"), {params: ""})
   }),
   new Rule(" ", /^(#{1,6}) $/, function(pm, match, pos) {
-    setAs(pm, pos, "heading", {level: match[1].length})
+    setAs(pm, pos, pm.schema.nodeType("heading"), {level: match[1].length})
   })
 ]
 
