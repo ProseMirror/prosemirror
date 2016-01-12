4c31572edd730a9ecd5dbef9927ad9672ef68b74
Marijn Haverbeke <marijnh@gmail.com>
2016-01-12 15:44:38 +0100

    docs(collab): Add doc comments to the collab module

--- src/collab/index.js
+++ src/collab/index.js
@@ -5,6 +5,15 @@ import {AssertionError} from "../util/error"
 import {rebaseSteps} from "./rebase"
 export {rebaseSteps}
 
+// :: ?Object #path=collab #kind=option
+//
+// When given, enables the collaborative editing framework for the
+// editor. Will register itself of the `Collab` class as
+// `.mod.collab`.
+//
+// If the object given has a `version` property, that will determine
+// the starting version number of the collaborative editing.
+
 defineOption("collab", false, (pm, value) => {
   if (pm.mod.collab) {
     pm.mod.collab.detach()
@@ -16,11 +25,24 @@ defineOption("collab", false, (pm, value) => {
   }
 })
 
+// ;; This class accumulates changes that have to be sent to the
+// central authority in the collaborating group, signals an event when
+// it has something to send, and makes it possible to integrate
+// changes made by peers into our local document. It is created and
+// attached to the editor (under `.mod.collab`) by setting the
+// `collab` option.
+//
+// Includes the [event mixin](#EventMixin).
 class Collab {
   constructor(pm, options) {
     this.pm = pm
     this.options = options
 
+    // :: number
+    // The version number of the last update received from the central
+    // authority. Starts at 0 or the value of the `version` property
+    // in the option object, for the editor's value when the option
+    // was enabled.
     this.version = options.version || 0
     this.versionDoc = pm.doc
 
@@ -32,6 +54,10 @@ class Collab {
         this.unconfirmedSteps.push(transform.steps[i])
         this.unconfirmedMaps.push(transform.maps[i])
       }
+      // :: () #path=Collab#events#mustSend
+      // Fired when there are new steps to send to the central
+      // authority. Consumers should respond by calling
+      // `sendableSteps` and pushing those to the authority.
       this.signal("mustSend")
     })
     pm.on("beforeSetDoc", this.onSetDoc = () => {
@@ -46,10 +72,15 @@ class Collab {
     this.pm.history.allowCollapsing = true
   }
 
+  // :: () -> bool
+  // Reports whether the editor has any unsent steps.
   hasSendableSteps() {
     return this.unconfirmedSteps.length > 0
   }
 
+  // :: () → {version: number, doc: Node, steps: [Step]}
+  // Provides the data describing the editor's unconfirmed steps. This
+  // is the thing you'd send to the central authority.
   sendableSteps() {
     return {
       version: this.version,
@@ -58,6 +89,10 @@ class Collab {
     }
   }
 
+  // :: ({version: number, doc: Node, steps: [Step]})
+  // Tells the module that a set of unconfirmed steps have been
+  // accepted by the central authority, and can now be considered
+  // confirmed.
   confirmSteps(sendable) {
     this.unconfirmedSteps.splice(0, sendable.steps.length)
     this.unconfirmedMaps.splice(0, sendable.steps.length)
@@ -65,6 +100,13 @@ class Collab {
     this.versionDoc = sendable.doc
   }
 
+  // :: ([Step]) → [PosMap]
+  // Pushes a set of steps (made by peers and received from the
+  // central authority) into the editor. This will rebase any
+  // unconfirmed steps over these steps.
+  //
+  // Returns the [position maps](#PosMap) produced by applying the
+  // steps.
   receive(steps) {
     let doc = this.versionDoc
     let maps = steps.map(step => {
