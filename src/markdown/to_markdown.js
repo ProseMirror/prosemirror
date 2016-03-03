871eda8a6d9ab26a6c87630ce94b80c960cb4921
Marijn Haverbeke <marijnh@gmail.com>
2016-03-03 09:53:43 +0100

    Fix bogus comparison in to_markdown
    The code still isn't great -- it hard-codes a mark name, which it
    should not be doing -- but at least it sort of works now.
    
    Issue #216

--- src/markdown/to_markdown.js
+++ src/markdown/to_markdown.js
@@ -123,8 +123,8 @@ class MarkdownSerializer {
     let stack = []
     let progress = node => {
       let marks = node ? node.marks.slice() : []
-      if (stack.length && stack[stack.length - 1].type == "code" &&
-          (!marks.length || marks[marks.length - 1].type != "code")) {
+      if (stack.length && stack[stack.length - 1].type.name == "code" &&
+          (!marks.length || marks[marks.length - 1].type.name != "code")) {
         this.text("`", false)
         stack.pop()
       }
