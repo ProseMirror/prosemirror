4378af9f7f1e6216f030ac8769451eba5d6dd9c8
Marijn Haverbeke <marijn@haverbeke.nl>
2016-05-19 14:28:29 +0200

    Add docstrings to orderedmap.js

--- src/util/orderedmap.js
+++ src/util/orderedmap.js
@@ -1,3 +1,5 @@
+// ;; Persistent data structure representing an ordered mapping from
+// strings to values, with some convenient update methods.
 export class OrderedMap {
   constructor(content) {
     this.content = content
@@ -9,18 +11,21 @@ export class OrderedMap {
     return -1
   }
 
-  addToStart(map) {
-    map = OrderedMap.from(map)
-    return new OrderedMap(map.content.concat(this.subtract(map).content))
+  // :: (string) → ?any
+  // Retrieve the value stored under `key`, or return undefined when
+  // no such key exists.
+  get(key) {
+    let found = this.find(key)
+    return found == -1 ? undefined : this.content[found + 1]
   }
 
-  addToEnd(map) {
-    map = OrderedMap.from(map)
-    return new OrderedMap(this.subtract(map).content.concat(map.content))
-  }
-
-  set(key, value, newKey) {
-    let found = this.find(key), content = this.content.slice()
+  // :: (string, any, ?string) → OrderedMap
+  // Create a new map by replacing the value of `key` with a new
+  // value, or adding a binding to the end of the map. If `newKey` is
+  // given, the key of the binding will be replaced with that key.
+  update(key, value, newKey) {
+    let self = newKey && newKey != key ? this.remove(newKey) : this
+    let found = self.find(key), content = self.content.slice()
     if (found == -1) {
       content.push(newKey || key, value)
     } else {
@@ -30,6 +35,8 @@ export class OrderedMap {
     return new OrderedMap(content)
   }
 
+  // :: (string) → OrderedMap
+  // Return a map with the given key removed, if it existed.
   remove(key) {
     let found = this.find(key)
     if (found == -1) return this
@@ -38,36 +45,65 @@ export class OrderedMap {
     return new OrderedMap(content)
   }
 
+  // :: (string, any) → OrderedMap
+  // Add a new key to the start of the map.
+  addToStart(key, value) {
+    return new OrderedMap([key, value].concat(this.remove(key).content))
+  }
+
+  // :: (string, any) → OrderedMap
+  // Add a new key to the end of the map.
+  addToEnd(key, value) {
+    let content = this.remove(key).content.slice()
+    content.push(key, value)
+    return new OrderedMap(content)
+  }
+
+  // :: ((key: string, value: any))
+  // Call the given function for each key/value pair in the map, in
+  // order.
+  forEach(f) {
+    for (let i = 0; i < this.content.length; i += 2)
+      f(this.content[i], this.content[i + 1])
+  }
+
+  // :: (union<Object, OrderedMap) → OrderedMap
+  // Create a new map by prepending the keys in this map that don't
+  // appear in `map` before the keys in `map`.
+  prepend(map) {
+    if (!map.size) return this
+    map = OrderedMap.from(map)
+    return new OrderedMap(map.content.concat(this.subtract(map).content))
+  }
+
+  // :: (union<Object, OrderedMap) → OrderedMap
+  // Create a new map by appending the keys in this map that don't
+  // appear in `map` after the keys in `map`.
+  append(map) {
+    if (!map.size) return this
+    map = OrderedMap.from(map)
+    return new OrderedMap(this.subtract(map).content.concat(map.content))
+  }
+
+  // :: (union<Object, OrderedMap) → OrderedMap
+  // Create a map containing all the keys in this map that don't
+  // appear in `map`.
   subtract(map) {
     let result = this
     OrderedMap.from(map).forEach(key => result = result.remove(key))
     return result
   }
 
-  forEach(f) {
-    for (let i = 0; i < this.content.length; i += 2)
-      f(this.content[i], this.content[i + 1])
-  }
-
-  get(key) {
-    let found = this.find(key)
-    return found == -1 ? undefined : this.content[found + 1]
-  }
-
-  setAtStart(key, value) {
-    return new OrderedMap([key, value].concat(this.remove(key).content))
-  }
-
-  setAtEnd(key, value) {
-    let content = this.remove(key).content.slice()
-    content.push(key, value)
-    return new OrderedMap(content)
-  }
-
+  // :: number
+  // The amount of keys in this map.
   get size() {
     return this.content.length >> 1
   }
 
+  // :: (?union<Object, OrderedMap) → OrderedMap
+  // Return a map with the given content. If null, create an empty
+  // map. If given an ordered map, return that map itself. If given an
+  // object, create a map from the object's properties.
   static from(value) {
     if (value instanceof OrderedMap) return value
     let content = []
