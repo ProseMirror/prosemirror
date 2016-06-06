ProseMirror/prosemirror

commit 0c7aff3753257ea3bbb544cd0d6a4856b4488c80

Fix a number of bugs where code assumed the doc node isn't a textblock
Issue #344

src/edit/selection.js
@@ -344,9 +344,9 @@ exports.hasFocus = hasFocus
 // position where the search starts. When `text` is true, only return
 // text selections.
 function findSelectionIn(doc, node, pos, index, dir, text) {
+  if (node.isTextblock) return new TextSelection(doc.resolve(pos + dir))
   for (let i = index - (dir > 0 ? 0 : 1); dir > 0 ? i < node.childCount : i >= 0; i += dir) {
     let child = node.child(i)
-    if (child.isTextblock) return new TextSelection(doc.resolve(pos + dir))
     if (!child.type.isLeaf) {
       let inner = findSelectionIn(doc, child, pos + dir, dir < 0 ? child.childCount : 0, dir, text)
       if (inner) return inner
@@ -402,7 +402,7 @@ exports.findSelectionAtEnd = findSelectionAtEnd
 // Whether vertical position motion in a given direction
 // from a position would leave a text block.
 function verticalMotionLeavesTextblock(pm, $pos, dir) {
-  let dom = DOMAfterPos(pm, $pos.before())
+  let dom = $pos.depth ? DOMAfterPos(pm, $pos.before()) : pm.content
   let coords = coordsAtPos(pm, $pos.pos)
   for (let child = dom.firstChild; child; child = child.nextSibling) {
     if (child.nodeType != 1) continue

src/edit/capturekeys.js
@@ -8,8 +8,8 @@ function nothing() {}
 function moveSelectionBlock(pm, dir) {
   let {$from, $to, node} = pm.selection
   let $side = dir > 0 ? $to : $from
-  let $start = node && node.isBlock ? $side : pm.doc.resolve(dir > 0 ? $side.after() : $side.before())
-  return findSelectionFrom($start, dir)
+  let $start = node && node.isBlock ? $side : $side.depth ? pm.doc.resolve(dir > 0 ? $side.after() : $side.before()) : null
+  return $start && findSelectionFrom($start, dir)
 }
 
 function selectNodeHorizontally(pm, dir) {

src/edit/input.js
@@ -353,8 +353,8 @@ handlers.input = pm => {
 
 function toClipboard(doc, from, to, dataTransfer) {
   let slice = doc.slice(from, to)
-  if (!slice.openLeft && !slice.openRight && slice.possibleParent)
-    slice = new Slice(Fragment.from(slice.possibleParent.copy(slice.content), 1, 1))
+  if (!slice.openLeft && !slice.openRight && slice.possibleParent && slice.possibleParent.type != doc.type.schema.nodes.doc)
+    slice = new Slice(Fragment.from(slice.possibleParent.copy(slice.content)), 1, 1)
   let dom = toDOM(slice.content), wrap = document.createElement("div")
   if (dom.firstChild && dom.firstChild.nodeType == 1)
     dom.firstChild.setAttribute("pm-open-left", slice.openLeft)

commit 740d259894361d6c2f372ebf1eb01be52bbb703f

Fix bug introduced in 0c7aff3753257ea3
Issue #344

src/edit/selection.js
@@ -344,7 +344,7 @@ exports.hasFocus = hasFocus
 // position where the search starts. When `text` is true, only return
 // text selections.
 function findSelectionIn(doc, node, pos, index, dir, text) {
-  if (node.isTextblock) return new TextSelection(doc.resolve(pos + dir))
+  if (node.isTextblock) return new TextSelection(doc.resolve(pos))
   for (let i = index - (dir > 0 ? 0 : 1); dir > 0 ? i < node.childCount : i >= 0; i += dir) {
     let child = node.child(i)
     if (!child.type.isLeaf) {