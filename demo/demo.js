39e0e9b116564d6cd6e7a97d28a72f4bc248e387
Marijn Haverbeke <marijn@haverbeke.nl>
2016-06-14 17:08:19 +0200

    Update demo for fromDOM -> schema.parseDOM change

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,4 @@
 var ProseMirror = require("../src/edit").ProseMirror
-var fromDOM = require("../src/htmlformat").fromDOM
 var schema = require("../src/schema").defaultSchema
 var tooltipMenu = require("../src/menu/tooltipmenu").tooltipMenu
 var schemaMenu = require("../src/schema/menu")
@@ -9,7 +8,7 @@ var menu = schemaMenu.defaultMenuItems(schema)
 
 var pm = window.pm = new ProseMirror({
   place: document.querySelector(".full"),
-  doc: fromDOM(schema, document.querySelector("#content")),
+  doc: schema.parseDOM(document.querySelector("#content")),
   plugins: [tooltipMenu.config({selectedBlockMenu: true,
                                 inlineContent: menu.inlineMenu,
                                 blockContent: menu.blockMenu}),
