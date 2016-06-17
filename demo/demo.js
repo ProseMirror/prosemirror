e47d1275e949d7d3963455b3f0ca13cef58f006f
Marijn Haverbeke <marijn@haverbeke.nl>
2016-06-17 13:25:08 +0200

    First draft of a schema-table module
    Issue #376

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,21 @@
-var ProseMirror = require("../src/edit").ProseMirror
-var schema = require("../src/schema-basic").schema
-var exampleSetup = require("../src/example-setup").exampleSetup
+const {ProseMirror} = require("../src/edit")
+const {Schema} = require("../src/model")
+const {schema} = require("../src/schema-basic")
+const {exampleSetup} = require("../src/example-setup")
+const {addTableNodes} = require("../src/schema-table")
 
-var pm = window.pm = new ProseMirror({
+const mySchema = new Schema({
+  nodes: addTableNodes(schema.nodeSpec, "block+"),
+  marks: schema.markSpec
+})
+
+const pm = window.pm = new ProseMirror({
   place: document.querySelector(".full"),
-  doc: schema.parseDOM(document.querySelector("#content")),
+  doc: mySchema.parseDOM(document.querySelector("#content")),
   plugins: [exampleSetup.config({tooltipMenu: true})]
 })
 
-document.querySelector("#mark").addEventListener("mousedown", function(e) {
+document.querySelector("#mark").addEventListener("mousedown", e => {
   pm.markRange(pm.selection.from, pm.selection.to, {className: "marked"})
   e.preventDefault()
 })
