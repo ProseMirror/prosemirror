53c96eb503820b923fc84c756089a66e2e590a46
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-09 11:48:41 +0200

    Restore the demo

---
+++ demo/demo.js
@@ -0,0 +1,20 @@
+const {Schema} = require("./parent/model")
+const {EditorState} = require("./parent/state")
+const {schema} = require("./parent/schema-basic")
+const {addListNodes} = require("./parent/schema-list")
+const {addTableNodes} = require("./parent/schema-table")
+const {MenuBarEditorView} = require("./parent/menu")
+const {exampleSetup} = require("./parent/example-setup")
+
+const demoSchema = new Schema({
+  nodes: addListNodes(addTableNodes(schema.nodeSpec, "block+", "block"), "paragraph block*", "block"),
+  marks: schema.markSpec
+})
+
+let state = EditorState.create({doc: demoSchema.parseDOM(document.querySelector("#content")),
+                                plugins: [exampleSetup({schema: demoSchema})]})
+
+let view = window.view = new MenuBarEditorView(document.querySelector(".full"), {
+  state,
+  onAction: action => view.updateState(view.editor.state.applyAction(action))
+})
