f107ee739b9b0980cf717353dd3ebfaacbfc9158
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-06 22:23:41 +0100

    Adjust demo to the fact that menuBar is now a plugin

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,9 @@
 const {Schema, DOMParser} = require("prosemirror-model")
+const {EditorView} = require("prosemirror-view")
 const {EditorState} = require("prosemirror-state")
 const {schema} = require("prosemirror-schema-basic")
 const {addListNodes} = require("prosemirror-schema-list")
 const {addTableNodes} = require("prosemirror-schema-table")
-const {MenuBarEditorView} = require("prosemirror-menu")
 const {exampleSetup} = require("prosemirror-example-setup")
 
 const demoSchema = new Schema({
@@ -14,5 +14,4 @@ const demoSchema = new Schema({
 let state = EditorState.create({doc: DOMParser.fromSchema(demoSchema).parse(document.querySelector("#content")),
                                 plugins: exampleSetup({schema: demoSchema})})
 
-let view = new MenuBarEditorView(document.querySelector(".full"), {state})
-window.view = view.editor
+let view = window.view = new EditorView(document.querySelector(".full"), {state})
