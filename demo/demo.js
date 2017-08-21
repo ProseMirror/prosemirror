7972ec73b9c80f816e55ebee91ba1765a64e4adf
Marijn Haverbeke <marijn@haverbeke.nl>
2017-08-21 17:20:30 +0200

    Remove deprecated schema-table module from demo

--- demo/demo.js
+++ demo/demo.js
@@ -3,11 +3,10 @@ const {EditorView} = require("prosemirror-view")
 const {EditorState, TextSelection} = require("prosemirror-state")
 const {schema} = require("prosemirror-schema-basic")
 const {addListNodes} = require("prosemirror-schema-list")
-const {addTableNodes} = require("prosemirror-schema-table")
 const {exampleSetup} = require("prosemirror-example-setup")
 
 const demoSchema = new Schema({
-  nodes: addListNodes(addTableNodes(schema.spec.nodes, "block+", "block"), "paragraph block*", "block"),
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
   marks: schema.spec.marks
 })
 
