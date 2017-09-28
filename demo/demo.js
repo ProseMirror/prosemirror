08b8a16f0860ebd1e4804617786ea58e94b18e65
Marijn Haverbeke <marijn@haverbeke.nl>
2017-09-28 11:58:35 +0200

    Remove accidentally commited node view in demo

--- demo/demo.js
+++ demo/demo.js
@@ -10,20 +10,7 @@ const demoSchema = new Schema({
   marks: schema.spec.marks
 })
 
-class ParagraphView {
-  constructor(node) {
-    this.dom = this.contentDOM = document.createElement("p")
-    if (node.content.size == 0) this.dom.classList.add("empty")
-  }
-
-  update(node) {
-    if (node.type.name != "paragraph") return false
-    if (node.content.size > 0) this.dom.classList.remove("empty")
-    else this.dom.classList.add("empty")
-    return true
-  }
-}
 let state = EditorState.create({doc: DOMParser.fromSchema(demoSchema).parse(document.querySelector("#content")),
                                 plugins: exampleSetup({schema: demoSchema})})
 
-let view = window.view = new EditorView(document.querySelector(".full"), {state, nodeViews: {paragraph(node, v, g) { return new ParagraphView(node, v, g) }}})
+let view = window.view = new EditorView(document.querySelector(".full"), {state})
